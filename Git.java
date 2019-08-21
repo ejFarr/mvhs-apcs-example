9f516b991bbcc368030406d9346f7279925c2c40
Evan Farr <roboproductions200@gmail.com>
2019-08-21 12:31:44 -0600

    common stupid, lacking common sense but not unintelligent

--- Git.java
+++ Git.java
@@ -6,6 +6,6 @@ public class Git
         System.out.println("Hi mom");
         System.out.println("Literature is an interesting subject though most people nowadays don't respect it, KYLE");
         System.out.println("Kyle stop asking questions");
-        
+        System.out.println("Kyle is common stupid");
     }
 }
